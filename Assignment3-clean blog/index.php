3143bcd6de954427ebf5f45eb1780bcf210dfac2
Yuanzhuo Wang <ywang249@u.rochester.edu>
2018-02-25 21:34:36 -0500

    nav

--- Assignment3-clean blog/index.php
+++ Assignment3-clean blog/index.php
@@ -2,32 +2,7 @@
 
   <body>
 
-    <!-- Navigation -->
-    <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
-      <div class="container">
-        <a class="navbar-brand" href="index.php">Idiotville</a>
-        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-          Menu
-          <i class="fa fa-bars"></i>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarResponsive">
-          <ul class="navbar-nav ml-auto">
-            <li class="nav-item">
-              <a class="nav-link" href="index.php">Home</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="alejandro_abreu.php">Alejandro Abreu</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="fiyinfoluwa_oluyinka.php">Fiyinfoluwa Oluyinka</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="jonathan_hirsch.php">Jonathan Hirsch</a>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </nav>
+   <?php include "inc/nav.inc"; ?>
 
     <!-- Page Header -->
     <header class="masthead" style="background-image: url('img/home-bg.jpg')">
