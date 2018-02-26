b72d84fcd2c791acd67359b911dd920646a7697c
Yuanzhuo Wang <ywang249@u.rochester.edu>
2018-02-25 22:07:41 -0500

    a

--- Assignment3-clean blog/alejandro_abreu.php
+++ Assignment3-clean blog/alejandro_abreu.php
@@ -23,12 +23,12 @@
     <div class="container">
       <div class="row">
         <div class="col-lg-8 col-md-10 mx-auto">
-          <a href="#">
-              <img class="img-fluid" src="img/alejandro_abreu.jpg" alt="">
-            </a>
+          
           <span class="caption text-muted">Alejandro Abreu</span>
 
-          <p>The first job that he obtained was here at the University of Rochester as a Building Manager at the Goergen Athletic Center. As a building manager at the Goergen Athletic Center, his main responsibilities include ensuring the safety of over 300 students and faculty members using the athletic facility. Building Managers take more than 3 tours a shift around the gym in order to ensure safety and proper usage of the equipment. In addition, a Building Manager is tasked with the responsibility of selling and promoting various membership packages. Finally, he also provided information of building and recreational activities to interested potential and active gym members over the phone or in person.</p>
+          <p><a href="#">
+              <img class="img-fluid" src="img/alejandro_abreu.jpg" alt="">
+            </a>The first job that he obtained was here at the University of Rochester as a Building Manager at the Goergen Athletic Center. As a building manager at the Goergen Athletic Center, his main responsibilities include ensuring the safety of over 300 students and faculty members using the athletic facility. Building Managers take more than 3 tours a shift around the gym in order to ensure safety and proper usage of the equipment. In addition, a Building Manager is tasked with the responsibility of selling and promoting various membership packages. Finally, he also provided information of building and recreational activities to interested potential and active gym members over the phone or in person.</p>
 
           <p>He has previously worked with Upward bound as tutor. Upward Bound main focus is to provide high school students with the resources and support they need to get to and through college. As a tutor for Upward Bound, he improved on his communication skills by being responsible for tutoring high school students individually and in small groups who struggled in math. He ensured that every student receives his undivided attention and grasp the material in order to complete their work. When not tasked with tutoring students, he completed an excel spreadsheet on students personal information. Tutoring for Upward Bound has given him some experience in office space environment.</p>
 
--- Assignment3-clean blog/jonathan_hirsch.php
+++ Assignment3-clean blog/jonathan_hirsch.php
@@ -25,12 +25,11 @@
         <div class="col-lg-8 col-md-10 mx-auto">
 
 
-          <a href="#">
-              <img class="img-fluid" src="img/jonathan_hirsch.jpg" alt="">
-          </a>
           <span class="caption text-muted">Jonathan Hirsch</span>
 
-          <p>His first “job” was a research experience over the summer of 2014. He got to be an assistant to Dr. Steve Danford at the University of North Carolina Greensboro (UNCG), and helped analyze spectroscopy data of B[e] class stars. Astro-Physics will always be a love of his, and being able to take part in that line of research was a huge opportunity for him.</p>
+          <p><a href="#">
+              <img class="img-fluid" src="img/jonathan_hirsch.jpg" alt="">
+          </a>His first “job” was a research experience over the summer of 2014. He got to be an assistant to Dr. Steve Danford at the University of North Carolina Greensboro (UNCG), and helped analyze spectroscopy data of B[e] class stars. Astro-Physics will always be a love of his, and being able to take part in that line of research was a huge opportunity for him.</p>
 
           <p>In the summer of 2017, he ended up with a job at Lowes in Mobile, Al as a sales associate. His duties included keeping shelves stocked and presentable, helping customers in his area find what they needed as well as pushing the sales of lawn mowers and grills on unsuspecting customers. He enjoyed having a job that summer, but it was not one he wanted to continue to have in the future.</p>
 
